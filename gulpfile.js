2bc000c371ff242f887a1524f6563e99c0fc93ad
Billy Everyteen <nikolasapayne@gmail.com>
2016-02-10 13:18:38 -0600

    initializing the repository

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    plum = require('gulp-plumber'),
+    sync = require('browser-sync'),
+    auto = require('gulp-autoprefixer');
+
+gulp.task('serve', ['sass'], function() {
+  sync({
+    proxy: 'localhost/sage-coming'
+  });
+  gulp.watch(['./*.html']).on('change', sync.reload);
+  gulp.watch(['./_*.scss','./scss/app.scss'],['sass']);
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./scss/app.scss')
+    .pipe(plum())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(auto({ browsers: ['last 2 versions'], cascade: false }))
+    .pipe(gulp.dest('./css'))
+    .pipe(sync.stream());
+});
+
+gulp.task('watch', ['serve'], function() {
+  gulp.watch('./scss/*.scss', ['sass']);
+});
+
+gulp.task('default', ['serve'], function() {});
